fce06ff775e59ec03137b1aad78d8ce6874eef1f
MaheshFSD <maheshfsdeveloper@gmail.com>
2024-07-23 21:01:28 +0530

    new user creation route is done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 const connectToDB = require('./config');
+const User = require('./models/user.model');
 
 connectToDB(process.env.mongourl)
 .then(() => console.log('DB Connection sucessfull...'))
@@ -15,9 +16,10 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
-
-
+app.post('/api/users', async (req,res) => {
+  const insertedDoc = await User.create({username: req.body.username})
+  res.json({username: req.body.username, _id: insertedDoc['_id']});
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
