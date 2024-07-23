368a408f6619a4841005e58aeda22c6eb61c4eb6
MaheshFSD <maheshfsdeveloper@gmail.com>
2024-07-23 20:26:50 +0530

    db connection done

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const connectToDB = require('./config');
+
+connectToDB(process.env.mongourl)
+.then(() => console.log('DB Connection sucessfull...'))
+.catch(err => console.log('Something went wrong while connecting to db...'));
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
