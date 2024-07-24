90171ad3b2a7d7cbc8596a9e65c590a6cf264c5b
MaheshFSD <maheshfsdeveloper@gmail.com>
2024-07-24 21:47:53 +0530

    another way of adding user to db

--- index.js
+++ index.js
@@ -16,10 +16,27 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+// user creation
+// app.post('/api/users', async (req,res) => {
+//   const insertedDoc = await User.create({username: req.body.username})
+//   res.json({username: req.body.username, _id: insertedDoc['_id']});
+// })
+
+// the other way of doing this insertion is as follows 
 app.post('/api/users', async (req,res) => {
-  const insertedDoc = await User.create({username: req.body.username})
-  res.json({username: req.body.username, _id: insertedDoc['_id']});
+  const user = new User({username: req.body.username})
+  try {
+    const insertedDoc = await user.save()
+    res.json({username: req.body.username, _id: insertedDoc['_id']});
+  } catch (error) {
+    console.log(error, ' ----------- occured in insertion -----');
+  }
 })
+
+// Add excercise details to a user
+// app.post('/api/users/:_id/exercises', async (req,res) => {
+//   const bodyData = 
+// })
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
---
+++ models/excercises.model.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const excercisSchema = new mongoose.Schema({
+    username: "fcc_test",
+    description: "test",
+    duration: 60,
+    date: "Mon Jan 01 1990",
+    _id: "5fb5853f734231456ccb3b05"
+})
